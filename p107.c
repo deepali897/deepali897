267c6102fdf4cd26af23f824da5b8804e9580313
deepali897 <routdeepali03@gmail.com>
2025-02-16 17:44:23 +0530

    Add files via upload

---
+++ p107.c
@@ -0,0 +1,38 @@
+// C program for implementation of ftoa() 
+#include <math.h> 
+#include <stdio.h> 
+ 
+// Reverses a string 'str' of length 'len' 
+void reverse(char* str, int len) 
+{ 
+    int i = 0, j = len - 1, temp; 
+    while (i < j) { 
+        temp = str[i]; 
+        str[i] = str[j]; 
+        str[j] = temp; 
+        i++; 
+        j--; 
+    } 
+} 
+ 
+// Converts a given integer x to string str[]. 
+// d is the number of digits required in the output. 
+// If d is more than the number of digits in x, 
+// then 0s are added at the beginning. 
+int intToStr(int x, char str[], int d) 
+{ 
+    int i = 0; 
+    while (x) { 
+        str[i++] = (x % 10) + '0'; 
+        x = x / 10; 
+    } 
+ 
+    // If number of digits required is more, then 
+    // add 0s at the beginning 
+    while (i < d) 
+        str[i++] = '0'; 
+ 
+    reverse(str, i); 
+    str[i] = '\0'; 
+    return i; 
+} 
